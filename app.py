de5e848cda3f7dc48459ae9dc1c71cbf9fb777fe
yehiaa2050-png <yehiaa2050@gmail.com>
2026-07-06 01:07:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-from fastapi import FastAPI
 
-app = FastAPI()
-
-@app.get("/")
-def greet_json():
-    return {"Hello": "Welcome to my N8N Automation Space!"}
-
-@app.get("/health")
-def health_check():
-    return {"status": "healthy"}
